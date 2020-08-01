29426f391b638bcc44c93ec996a40ebcadf297f8
hanyoungjae <han6318@naver.com>
2020-08-02 01:06:38 +0900

    feat: update prettier & eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
+const { rules } = require('eslint-config-prettier');
+
 module.exports = {
   root: true,
   plugins: ['prettier'],
-  extends: ['@react-native-community', 'prettier', 'prettier/@typescript-eslint']
+  extends: ['@react-native-community', 'prettier', 'prettier/@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
 };
